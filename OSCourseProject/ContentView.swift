2fc90d38fb030fcbf610394f4c7da2d07d242a78
Concerto <zxysb0@qq.com>
2024-06-03 08:45:20 +0800

    Initial Commit

---
+++ OSCourseProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OSCourseProject
+//
+//  Created by 张炫阳 on 2024/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OSCourseProject/OSCourseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSCourseProjectApp.swift
+//  OSCourseProject
+//
+//  Created by 张炫阳 on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct OSCourseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
